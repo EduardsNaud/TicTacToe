eba3f8ac8c29b7fe6928abe81acf5ade0773ad65
EduardsNaud <97410974+EduardsNaud@users.noreply.github.com>
2022-03-21 10:18:40 +0200

    Informacija

--- spele.py
+++ spele.py
@@ -6,6 +6,50 @@ mansLogs.title('TicTacToe')
 speletajsX=True #Tas kurš liks krustiņus
 count=0 #aizpildīto rūtiņu skaits
 
+def resets():
+    btn1.config(state=NORMAL)
+    btn2.config(state=NORMAL)
+    btn3.config(state=NORMAL)
+    btn4.config(state=NORMAL)
+    btn5.config(state=NORMAL)
+    btn6.config(state=NORMAL)
+    btn7.config(state=NORMAL)
+    btn8.config(state=NORMAL)
+    btn9.config(state=NORMAL)
+
+    btn1["text"]=""
+    btn2["text"]=""
+    btn3["text"]=""
+    btn4["text"]=""
+    btn5["text"]=""
+    btn6["text"]=""
+    btn7["text"]=""
+    btn8["text"]=""
+    btn9["text"]=""
+
+    global winner, count, speletajsX
+    winner=False
+    count=0
+    speletajsX=True
+    return 0
+
+
+def infoLogs():
+    jaunsLogs=Toplevel()
+    jaunsLogs.title('Info par programmu')
+    jaunsLogs.geometry("412x300")
+    apraksts=Label(jaunsLogs,text="Spēles noteikumi")
+    apraksts.grid(row=0,column=0)
+    apraksts=Label(jaunsLogs,text="1. Spēle tiek spēlēta 3x3 režģī")
+    apraksts.grid(row=1,column=0)
+    apraksts=Label(jaunsLogs,text="2. Pirmais sāk spēlēt X spēlētājs")
+    apraksts.grid(row=2,column=0)
+    apraksts=Label(jaunsLogs,text="3. Uzvar spelētājs kurš iegūst trīs lauciņus horizontāli,vertikāli vai pa diagonāli")
+    apraksts.grid(row=3,column=0)
+    return 0
+
+
+
 def disableButtons():
  btn1.config(state=DISABLED) #DISABLED = [poga izslēgta]
  btn2.config(state=DISABLED)
@@ -16,6 +60,7 @@ def disableButtons():
  btn7.config(state=DISABLED)
  btn8.config(state=DISABLED)
  btn9.config(state=DISABLED)
+
 
 def btnClick(button): #tukša poga tiek padota
     global speletajsX,count #mainīgie kuri tiks izmantoti
@@ -81,5 +126,18 @@ btn6.grid(row=1,column=2)
 btn7.grid(row=2,column=0)
 btn8.grid(row=2,column=1)
 btn9.grid(row=2,column=2)
+
+#IZVELNES VEIDOŠANA----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+galvenaIzvelne=Menu(mansLogs)#Izveido galveno izvelni
+mansLogs.config(menu=galvenaIzvelne)#pievieno galveno logu
+#MAZĀ IZVELNE----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+opcijas=Menu(galvenaIzvelne,tearoff=False)#mazā izvēlne
+galvenaIzvelne.add_cascade(label="Iestatijumi",menu=opcijas)#lejupkrītošais saraksts
+#KOMANDAS----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+opcijas.add_command(label="Jauna spēle",command=resets)
+opcijas.add_command(label="Iziet",command=mansLogs.quit)
+#PAPILDUS POGA IZVEIDE---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+galvenaIzvelne.add_command(label="Par programmu",command=infoLogs) # pievieno mazajai izvēlnei
+
 
 mansLogs.mainloop()
